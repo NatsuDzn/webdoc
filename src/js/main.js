af6e75af89efaf96a2cdf8d8c7bd50b661cacec2
NatsuDzn <natsuxgraph@gmail.com>
2019-06-21 18:31:03 +0200

    js cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,54 @@
 // Get the video element with id="myVideo"
 var vid = document.getElementById("video1");
+var juice = document.getElementById("orange-juice");
 
-var theVideo = document.getElementById("video1");
-  document.onkeydown = function(event) {
-      switch (event.keyCode) {
-         case 37:
-              event.preventDefault();
-              
-              vid_currentTime = theVideo.currentTime;
-              theVideo.currentTime = vid_currentTime - 5;
-         
-         case 39:
-              event.preventDefault();
-              
-              vid_currentTime = theVideo.currentTime;
-              theVideo.currentTime = vid_currentTime + 5;
-         
+document.onkeydown = function(event) {
+  switch (event.keyCode) {
+    case 37:
+      event.preventDefault();
+
+      vid_currentTime = vid.currentTime;
+      vid.currentTime -= parseInt(5);
+
+    case 39:
+      event.preventDefault();
+
+      vid_currentTime = vid.currentTime;
+      vid.currentTime = vid_currentTime + 5;
+
+    case 32:
+      event.preventDefault();
+
+      {
+        if (vid.paused) vid.play();
+        else vid.pause();
       }
-  };
-
+  }
+};
 
 // Assign an ontimeupdate event to the video element, and execute a function if the current playback position has changed
-vid.ontimeupdate = function() {annotation()};
+vid.ontimeupdate = function() {
+  annotation();
+};
 
 function annotation() {
-    // Display the current position of the video in a p element with id="demo"
-    document.getElementById("demo").innerHTML = vid.currentTime;
+  // Display the current position of the video in a p element with id="demo"
+  document.getElementById("demo").innerHTML = vid.currentTime;
 
-    var juice = document.getElementById("orange-juice");
-
-    vid.addEventListener('timeupdate', function(){
+  vid.addEventListener("timeupdate", function() {
     var juicePos = vid.currentTime / vid.duration;
     juice.style.width = juicePos * 100 + "%";
-})
-    
-    if((vid.currentTime >= 1) && (vid.currentTime <= 5)){
-    	document.getElementById('modal1').style.opacity = '100';
-    }else{
-    	document.getElementById('modal1').style.opacity = '0';
-    }
+  });
 
-    if((vid.currentTime >= 8) && (vid.currentTime <= 15)){
-    	document.getElementById('modal2').style.opacity = '100';
-    }else{
-    	document.getElementById('modal2').style.opacity = '0';
-    }
+  if (vid.currentTime >= 1 && vid.currentTime <= 5) {
+    document.getElementById("modal1").style.opacity = "100";
+  } else {
+    document.getElementById("modal1").style.opacity = "0";
+  }
+
+  if (vid.currentTime >= 8 && vid.currentTime <= 15) {
+    document.getElementById("modal2").style.opacity = "100";
+  } else {
+    document.getElementById("modal2").style.opacity = "0";
+  }
 }
