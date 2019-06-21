790d943b8a3838a52140a4d51226a3114e49d7f5
Nathanael <natsuxgraph@gmail.com>
2019-06-21 14:30:17 +0200

    add section & responsive

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal(".bgImgleft", { delay: 150 });
+ScrollReveal().reveal(".bgImgright", { delay: 150 });
+ScrollReveal().reveal(".bgImglast", { delay: 150 });
+ScrollReveal().reveal(".text", { delay: 250 });
